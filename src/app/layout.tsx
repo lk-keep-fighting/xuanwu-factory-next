c0eeb51252d2bfecfa7bb5dc71e017917047140a
lk <442969153@qq.com>
2025-11-26 17:03:13 +0800

    doc:改名字

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  title: "玄武工厂平台",
-  description: "云原生应用管理平台",
+  title: "玄武工厂研发管理平台",
+  description: "软件开发、测试、部署、发布",
   icons: {
     icon: "/logo.svg",
   },
